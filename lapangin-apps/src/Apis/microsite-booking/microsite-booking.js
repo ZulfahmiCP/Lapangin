96db6df43c10d2654ce86548c20debe5201dd002
Zulfahmi <142126150+ZulfahmiCP@users.noreply.github.com>
2025-12-06 22:11:54 +0800

    feat: create microsite booking api service

---
+++ lapangin-apps/src/Apis/microsite-booking/microsite-booking.js
@@ -0,0 +1,42 @@
+
+import api from '@/utils/axiosClient/axiosPublic.js';
+async function initializeBooking({courtId, selectedDate, venueId, selectedSchedule, idenpotenKey}) {
+    // const courtId = req.params.courtId;
+    // const date = req.params.selectedDate;
+    // const venueId = req.params.venueId;
+    // let selectedSchedule = req.body.selectedSchedule; // list of start time & end time 
+    // const idenpotenKey = req.body.idenpotenKey; // 
+
+    return await api.post(`/microsite_booking/initialize_booking/${venueId}/${courtId}/${selectedDate}`, {
+        selectedSchedule: selectedSchedule,
+        idenpotenKey: idenpotenKey
+    })
+
+}
+async function getBookingDetail(venueId, bookingId){
+    return await api.get(`/microsite_booking/get_booking/${venueId}/${bookingId}`);
+}
+async function initializePayment(fd) {
+    return await api.post(`/microsite_booking/initialize_payment`, fd, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    })
+
+}
+async function checkBookingStatus(bookingId){
+    return await api.get(`/microsite_booking/check_booking_status/${bookingId}`);
+}
+
+export {
+    initializeBooking,
+    getBookingDetail,
+    initializePayment,
+    checkBookingStatus
+}
+export default {
+    initializeBooking,
+    getBookingDetail,
+    initializePayment,
+    checkBookingStatus
+}
