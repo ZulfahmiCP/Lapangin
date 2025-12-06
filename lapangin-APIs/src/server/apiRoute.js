355fb2b0e9df3de3170c95ad8e10d9d7a99acc22
King Ahmad <kingahmadilyas03@gmail.com>
2025-12-06 11:15:19 +0800

    chore: setup supabase connection client

---
+++ lapangin-APIs/src/server/apiRoute.js
@@ -0,0 +1,44 @@
+import express from 'express';
+
+// import all entry point for every request
+import AvailabilityExceptionControllers from '../modules/availability/availabilityExceptionControllers.js';
+import AvailabilityController from '../modules/availability/availabilityRulesControllers.js';
+import CourtController from '../modules/courts/courtsControllers.js';
+import UserVenuesController from '../modules/venues/userVenuesControllers.js';
+import VanueController from '../modules/venues/venuesControllers.js';
+import SlotAvailability from '../modules/courts/slotAvailability.js';
+import TeamController from '../modules/team/teamControllers.js';
+import AdminPayment from '../modules/payment/adminPaymentControllers.js';
+
+
+// microsites
+import CourtMicrositeController from '../modules/microsites/courtMicrositeControllers.js';
+import AvaillabilityRules from '../modules/availability/availabilityRulesControllers.js';
+import MicrositeBooking from '../modules/microsite-booking/microsite_booking.js'
+
+
+
+
+const route = express.Router();
+
+// admin dashboard
+route.use('/venue', VanueController);
+route.use('/user_venues', UserVenuesController);
+route.use('/court', CourtController);
+route.use('/availabilityRules', AvailabilityController);
+route.use('/availabilityException', AvailabilityExceptionControllers);
+route.use('/courtAvailability', SlotAvailability); // admin court
+route.use('/team', TeamController);
+// admin payment
+route.use('/admin_payment', AdminPayment);
+
+// microsites (all request to these path will be available publicly)
+route.use('/courtMicrosite', CourtMicrositeController); // microsite
+route.use('/publicAvailability', AvaillabilityRules)
+route.use('/microsite_booking', MicrositeBooking);
+
+
+// TODO: venueCourt route entry point
+
+
+export default route;
